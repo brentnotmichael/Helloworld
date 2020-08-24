fca0dca5b1763e684e6df9b9fe8d8e9e97af86cd
Brent Gannetta <mgannett@mail.ccsf.edu>
2020-08-24 11:01:24 -0700

    Initial commit

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+// Testing github
+public class Helloworld
+{
+    // Prints "Hello, World!"
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
